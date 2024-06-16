d54d9cad3df698a5a1736a006eaba9a2e5f6999d
OlshaMB <michaelolshanskiy@icloud.com>
2024-06-16 22:32:00 +0300

    Adds builder

--- sandbox.ts
+++ sandbox.ts
@@ -1,33 +1,50 @@
+import { service } from "./src/builder.js";
 import { client, jsonBodyEncoder } from "./src/client.js";
-import { ActionReturnType, _ActionParams } from "./src/types.js";
-const handlers = {
-    "GET getUser": async (p: { id: string }, _body: null, _req: Request) => {
-        return {
-            body: {
-                user: {
-                    id: p.id,
-                    name: "Amy"
-                }
-            }
-        };
-    },
-    "POST postForm": async (p: { id: string }, body: Record<string, string>) => {
-        // if (!body.password || body.password != "password") return badRequestResponse();
-        return {
-            status: 200,
-            statusText: "Ok",
-            body: "Ok"
-        };
-    }
+import { ActionReturnType, actionHandler } from "./src/index.js";
+//server
+const getUser = async (p: { id: string }, _body: null, _req: Request) => {
+	return {
+		body: {
+			user: {
+				id: p.id,
+				name: "Amy"
+			}
+		}
+	};
 };
-const action = client<typeof handlers>(new URL("/"))
+const postForm = async (_p: { id: string }, body: Record<string, string>) => {
+	if (!body.password || body.password != "password") return {
+		status: 400,
+		statusText: "Bad request.",
+		body: "Bad request."
+	};
+	return {
+		status: 200,
+		statusText: "Ok",
+		body: "Ok"
+	};
+};
+const app = service()
+	.action("GET", "getUser", getUser)
+	.action("POST", "postForm", postForm)
+	.build();
+const _appHandler = actionHandler(app);
+// Hono app handler
+// ...
+export type AppService = typeof app;
+
+
+//client
+const action = client<AppService>(new URL("/"))
 const result = await action({
     method: "GET",
     action: "getUser",
     params: { id: "amy" },
     body: null,
 })
-if(result.ok) {const json = (await result.result.json()) as ActionReturnType<typeof handlers, "GET", "getUser">;}
+if(result.ok) {
+    const _json = (await result.result.json()) as ActionReturnType<AppService, "GET", "getUser">;
+}
 const result2 = await action({
     method: "POST",
     action: "postForm",
@@ -35,4 +52,6 @@ const result2 = await action({
     body: { password: "password" },
     bodyEncoder: jsonBodyEncoder
 })
-if(result2.ok) {const string = (await result2.result.text()) as ActionReturnType<typeof handlers, "POST", "postForm">;}
+if(result2.ok) {
+    const _string = (await result2.result.text()) as ActionReturnType<AppService, "POST", "postForm">;
+}
---
+++ src/builder.ts
@@ -0,0 +1,19 @@
+import { Action, ActionMethods } from "./types.js";
+
+export const service = <T extends object>(obj?: T)=>{
+    return {
+        action<M extends ActionMethods, N extends string, A extends Action<any, any, any>>(
+            method: M,
+            actionName: N,
+            actionCallback: A
+        ) {
+            return service({
+                ...(obj ?? {}),
+                [`${method} ${actionName}`]: actionCallback
+            } as T & { [P in `${M} ${N}`]: typeof actionCallback }) ;
+        },
+        build() {
+            return obj!;
+        }
+    };
+};
