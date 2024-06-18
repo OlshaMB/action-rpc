af2a1eb4ae0d92fa7fab3cfc992ed8c345dd3650
OlshaMB <michaelolshanskiy@icloud.com>
2024-06-18 14:33:59 +0300

    some formatting + adds headers where needed

--- sandbox.ts
+++ sandbox.ts
@@ -37,21 +37,26 @@ export type AppService = typeof app;
 //client
 const action = client<AppService>(new URL("/"))
 const result = await action({
-    method: "GET",
-    action: "getUser",
-    params: { id: "amy" },
-    body: null,
+	method: "GET",
+	action: "getUser",
+	params: { id: "amy" },
+	body: null,
 })
-if(result.ok) {
-    const _json = (await result.result.json()) as ActionReturnType<AppService, "GET", "getUser">;
+if (result.ok) {
+	const _json = (await result.result.json()) as ActionReturnType<AppService, "GET", "getUser">;
 }
 const result2 = await action({
-    method: "POST",
-    action: "postForm",
-    params: { id: "amy" },
-    body: { password: "password" },
-    bodyEncoder: jsonBodyEncoder
+	method: "POST",
+	action: "postForm",
+	params: { id: "amy" },
+	requestInit: {
+		headers: {
+			"Content-Type": "application/json"
+		},
+	},
+	body: { password: "password" },
+	bodyEncoder: jsonBodyEncoder
 })
-if(result2.ok) {
-    const _string = (await result2.result.text()) as ActionReturnType<AppService, "POST", "postForm">;
+if (result2.ok) {
+	const _string = (await result2.result.text()) as ActionReturnType<AppService, "POST", "postForm">;
 }
