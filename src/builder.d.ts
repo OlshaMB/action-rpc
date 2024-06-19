aed640e54655142657b6a5f0292d8e47c9fbd3e9
OlshaMB <michaelolshanskiy@icloud.com>
2024-06-19 20:45:21 +0300

    Fix builder action method returning wrong type(not builder itself).

--- src/builder.d.ts
+++ src/builder.d.ts
@@ -1,10 +1,11 @@
 import { Action, ActionMethods } from "./types.js";
 
-export declare function service<T extends object>(obj?: T): {
+type ServiceBuilder<T extends object> = {
     action<M extends ActionMethods, N extends string, A extends Action<any, any, any>>(
         method: M,
         actionName: N,
         actionCallback: A
-    ): T & { [P in `${M} ${N}`]: typeof actionCallback };
+    ): ServiceBuilder<T & { [P in `${M} ${N}`]: typeof actionCallback }>;
     build(): T;
-};
+}
+export declare function service<T extends object>(obj?: T): ServiceBuilder<T>;
